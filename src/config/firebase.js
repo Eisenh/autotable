8451baccf5773b0b741fe8e2863aa41a29cdb2f0
Eisenh <Eisenh@users.noreply.github.com>
2020-03-03 08:31:48 -0700

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -4,13 +4,7 @@ import 'firebase/firestore'
 function initFirebase () {
   Firebase.initializeApp({
     /* your Firebase config */
-    apiKey: 'AIzaSyD5Ng5aS4ht-mJb5etoePfgPcZaRh2ihPM',
-    authDomain: 'scratchpad-a8062.firebaseapp.com',
-    databaseURL: 'https://scratchpad-a8062.firebaseio.com',
-    projectId: 'scratchpad-a8062',
-    storageBucket: 'scratchpad-a8062.appspot.com',
-    messagingSenderId: '730444926930',
-    appId: '1:730444926930:web:e89e05335e28395f'
+  
   })
   return new Promise((resolve, reject) => {
     Firebase.firestore().enablePersistence()
